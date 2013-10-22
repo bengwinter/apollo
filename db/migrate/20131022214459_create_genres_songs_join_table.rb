a445d482d258b17d2e6bd315c5bdd8d53b296d8d
Ben Winter <ben.g.winter@gmail.com>
2013-10-22 17:49:25 -0400

    added join table for songs and genres. added seed data for genres

--- db/migrate/20131022214459_create_genres_songs_join_table.rb
+++ db/migrate/20131022214459_create_genres_songs_join_table.rb
@@ -1,4 +1,11 @@
 class CreateGenresSongsJoinTable < ActiveRecord::Migration
   def change
+    create_table :songs_genres, id: false do |t|
+      t.integer :song_id
+      t.integer :genre_id
+    end
+  add_index :songs_genres, :song_id
+  add_index :songs_genres, :genre_id
+  add_index :songs_genres, [:song_id, :genre_id]
   end
 end
