bf4af5817e4bfff6861e5f1bd8bf93409424faec
Ben Winter <ben.g.winter@gmail.com>
2013-10-22 21:28:03 -0400

    nested routes for songs playlists

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Apollo::Application.routes.draw do
   root to: 'home#index'
 
   devise_for :users
-
+  
   resources :songs
-  resources :playlists
+  
+  resources :playlists do
+    resources :songs
+  end
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
