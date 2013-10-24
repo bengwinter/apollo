9bb2bf2ac4acf103ff218d8e6eda6943ef8858d3
Ben Winter <ben.g.winter@gmail.com>
2013-10-24 13:56:25 -0400

    Double pipes

--- config/initializers/soundcloud.rb
+++ config/initializers/soundcloud.rb
@@ -1,4 +1,4 @@
-soundcloud_client_id = ENV['SOUNDCLOUD_CLIENT_ID'] | ""
+soundcloud_client_id = ENV['SOUNDCLOUD_CLIENT_ID'] || ""
 
 SOUNDCLOUD_CLIENT = SoundCloud.new({
   client_id: soundcloud_client_id
