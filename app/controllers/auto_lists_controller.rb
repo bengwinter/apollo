52a835c95d4f737dc5d44720048e9cb019e9e6ae
Ben Winter <ben.g.winter@gmail.com>
2013-10-26 11:07:35 -0400

    autolists controller working. going on to views next

--- app/controllers/auto_lists_controller.rb
+++ app/controllers/auto_lists_controller.rb
@@ -3,7 +3,7 @@ class AutoListsController < ApplicationController
   def genre
     # params.require(:genre).permit(:genre_name)
     binding.pry
-    genre = params[:id].capitalize
+    genre = params[:genre_name].capitalize
     @genre_playlist = Genre.where(name: genre).first
   end
 
