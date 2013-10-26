b59e39c7db380e377c30fce422bb280547a235ed
Ben Winter <ben.g.winter@gmail.com>
2013-10-26 17:07:57 -0400

    favoriting/unfavoriting from genre pages is now working

--- app/controllers/auto_lists_controller.rb
+++ app/controllers/auto_lists_controller.rb
@@ -1,8 +1,6 @@
 class AutoListsController < ApplicationController
 
   def genre
-    # params.require(:genre).permit(:genre_name)
-    binding.pry
     genre = params[:genre_name].capitalize
     @genre_playlist = Genre.where(name: genre).first
   end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,6 @@
  class SongsController < ApplicationController
-  before_action :set_song, only: [:show, :edit, :update, :destroy, :add_favorite, :remove_favorite]
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+  before_action :fav_song, only: [:add_favorite, :remove_favorite]
 
   def index
   end
@@ -41,7 +42,7 @@
   def update
     respond_to do |format|
       if @song.update(update_song_params)
-        format.html { redirect_to   ), notice: 'Song was successfully updated.' }
+        format.html { redirect_to playlist_path(playlist_id), notice: 'Song was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -60,15 +61,29 @@
 
   def add_favorite
     @song.favorite_add(params[:id])
-    current_user.playlists.find_by_name("Favorites").songs << @song
-    current_user.playlists.find_by_name("All").songs << @song
-    redirect_to playlist_path(playlist_id)
+    fav = current_user.playlists.find_by_name("Favorites")
+    fav.songs << @song unless fav.songs.include?(@song) 
+    all = current_user.playlists.find_by_name("All")
+    all.songs << @song unless all.songs.include?(@song)
+    if params[:playlist_id]
+      redirect_to playlist_path(params[:playlist_id])
+    elsif params[:genre_name]
+      redirect_to genre_page_path(params[:genre_name])
+    else
+      redirect_to root_path
+    end
   end
 
   def remove_favorite
     @song.favorite_remove(params[:id])
     current_user.playlists.find_by_name("Favorites").songs.delete(params[:id])
-    redirect_to playlist_path(playlist_id)
+    if params[:playlist_id]
+      redirect_to playlist_path(params[:playlist_id])
+    elsif params[:genre_name]
+      redirect_to genre_page_path(params[:genre_name])
+    else
+      redirect_to root_path
+    end
   end
 
   private
@@ -76,6 +91,10 @@
     def set_song
       @song = Song.find(params[:id])
       @playlist = Playlist.find(params[:playlist_id])
+    end
+
+    def fav_song
+      @song = Song.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
